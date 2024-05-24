2af3a1ff2ec762c3841e1ed6979b705679ad195c
Your Name <you@example.com>
2024-05-24 10:57:42 -0500

    exercio de front-end

---
+++ js/jquery-script.js
@@ -0,0 +1,9 @@
+//efeito de esconder formulario de cadastro
+
+$(document).ready(function(){
+    $("#botao-cadastrar").click(function(){
+    $("#form-cadastrar").slideToggle("slow");
+    $("#section-login").slideToggle("slow");
+    $("#botao-cadastrar").hide();
+    });
+});
